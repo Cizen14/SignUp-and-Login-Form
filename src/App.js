e9e456934ce9c48ef790221be69f2c043491d83b
Cizen14 <cizenmah@gmail.com>
2023-12-13 11:25:42 +1100

    Link with the singup Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+
+import SignUp from './SignUp';
 import Login from './Login';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    < div>
-     <Login/>
-    </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}></Route>
+        <Route path='/signup' element={ <SignUp/>}></Route>
+      </Routes>
+    </BrowserRouter>
+ 
   );
 }
 
